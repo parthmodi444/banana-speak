63db61f4396037119817850b2f62412b07cfc343
parthmodi444 <88581955+parthmodi444@users.noreply.github.com>
2021-09-12 01:23:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,30 @@ var txtInput=document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#output");
 var btnTranslate=document.querySelector('#btn-translate');
 btnTranslate.addEventListener("click",clickHandler);
+// var serverUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverUrl="https://api.funtranslations.com/translate/dolan.json";
+function getTranslationUrl(text)
+{
+    return serverUrl +"?"+"text="+text;
+}
+function errorHandler(error)
+{
+    console.log("error occured",error)
+}
 function clickHandler()
 {
-    alert("clicked");
-    outputDiv.innerHTML="translated text is "+txtInput.value;
-}
+ var inputText=txtInput.value;
+ fetch(getTranslationUrl(inputText))
+ .then(response =>response.json())
+ .then(json =>
+    {
+        var translatedText=json.contents.translated;
+        outputDiv.innerText=translatedText;
+    }
+    )
+ .catch(errorHandler)
+};
+
+
+
